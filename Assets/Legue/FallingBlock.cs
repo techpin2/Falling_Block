756be452d7ade16581d335f9edd7d12d3cde225a
techpin2 <89578919+techpin2@users.noreply.github.com>
2021-09-02 12:48:30 +0530

    Script Add
    A new Script Added FallingBlock

---
+++ Assets/Legue/FallingBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    float speed = 5;
+    private void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
