a9413e7fcec69c4e45a3007491548c79edbb2fc9
techpin2 <89578919+techpin2@users.noreply.github.com>
2021-09-02 13:37:25 +0530

    Fix Some Bugs

--- Assets/Legue/FallingBlock.cs
+++ Assets/Legue/FallingBlock.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class FallingBlock : MonoBehaviour
 {
-    float speed = 5;
+    public Vector2 speedMinMax;
+    float speed;
+    float visibleHeightThreshold;
+    private void Start()
+    {
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.difficulty.GetDifficultyPercent());
+        visibleHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;
+    }
     private void Update()
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+        if (transform.position.y <  visibleHeightThreshold)
+        {
+            Destroy(gameObject);
+        }
     }
 }
